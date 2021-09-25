5cdc2f53be938b403c43df9301d1800c987d33b7
Enchan <enchant0130@gmail.com>
2021-09-25 17:03:51 +0900

    [Add] main.swift, main-class

---
+++ Sources/RPiFrameBufferRepresentor/RPiFrameBufferRepresentor.swift
@@ -0,0 +1,20 @@
+//
+//  RPiFrameBufferRepresentor.swift
+//  RPiFrameBufferRepresentor
+//
+//  Created by EnchantCode on 2021/09/25.
+//
+
+import Foundation
+
+final class RPiFrameBufferRepresentor{
+    
+    final func main(arguments: [String]) -> Int32 {
+        
+        
+        
+        
+        return 0
+    }
+    
+}
--- Sources/RPiFrameBufferRepresentor/main.swift
+++ Sources/RPiFrameBufferRepresentor/main.swift
@@ -1,1 +1,17 @@
-print("Hello, world!")
+//
+//  main.swift
+//  entry-point
+//
+//  Created by EnchantCode on 2021/02/11.
+//
+
+import Foundation
+import CoreImage
+
+let arguments = CommandLine.arguments
+DispatchQueue.global().async {
+    let result = RPiFrameBufferRepresentor().main(arguments: arguments)
+    exit(result)
+}
+
+dispatchMain()
